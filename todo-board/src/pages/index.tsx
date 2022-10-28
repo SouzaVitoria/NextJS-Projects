e1d76e8b07f528e2f8cd175d3cf7759559fb1809
SouzaVitoria <viihsouza161@gmail.com>
2022-10-28 08:16:50 -0300

    todo-board: added _document and head title in page

--- todo-board/src/pages/index.tsx
+++ todo-board/src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head'
 import styles from "../styles/home.module.scss"
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}> Initial <span>NextJS</span></h1>
-    </div>
+    <>
+      <Head>
+        <title>Board - Organizando tarefas</title>
+      </Head>
+      <div>
+        <h1 className={styles.title}> Initial <span>NextJS</span></h1>
+      </div>
+    </>
   )
 }
