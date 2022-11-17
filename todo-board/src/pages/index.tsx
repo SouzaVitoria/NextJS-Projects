c80c414cfd0cc9a0f63d268d8ab0595add32f165
SouzaVitoria <viihsouza161@gmail.com>
2022-11-17 08:26:03 -0300

    todo-board: added Home page

--- todo-board/src/pages/index.tsx
+++ todo-board/src/pages/index.tsx
@@ -1,4 +1,9 @@
 import Head from 'next/head'
+import Image from 'next/image'
+
+import boardUserImage from "../../public/images/board-user.svg"
+import profileImage from "../../public/images/profileImage.png"
+
 import styles from "../styles/home.module.scss"
 
 export default function Home() {
@@ -7,9 +12,22 @@ export default function Home() {
       <Head>
         <title>Board - Organizando tarefas</title>
       </Head>
-      <div>
-        <h1 className={styles.title}> Initial <span>NextJS</span></h1>
-      </div>
+      <main className={styles.contentContainer}>
+        <Image src={boardUserImage} alt="Imagem de Ferramenta Board" />
+
+        <section className={styles.callToAction}>
+          <h1>Uma ferramenta para seu dia a dia. Escreva, planeja e organize-se...</h1>
+          <p>
+            <span> 100% Gratuita</span> e online
+          </p>
+        </section>
+        <div className={styles.donaters}>
+          <strong>Apoiadores:</strong>
+          <div>
+            <Image src={profileImage} alt="Usuário 1" />
+          </div>
+        </div>
+      </main>
     </>
   )
 }
